af0d3516c64381e96f5f48eb218099fb7d171b18
Doug Zobel <zobel_junk@hotmail.com>
2018-03-10 09:46:17 -0600

    Python script to send data up
    Python script to read scale data and send it up to AWS IOT via MQTT.

---
+++ scale.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python3
+import sys
+import time
+import json
+import statistics
+import datetime
+import os.path
+import RPi.GPIO as GPIO
+from hx711 import HX711  # https://github.com/tatobari/hx711py
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+LOGFILE = "/var/log/scale.log"
+ENDPOINT = "YOUR_AWS_ENDPOINT.iot.REGION.amazonaws.com"
+GROUP = "YOUR_GROUP_NAME"
+THING = "YOUR_THING_NAME"
+
+## Debug mode (verbose)
+#FAST_SAMPLE_PERIOD = 1
+#SLOW_SAMPLE_PERIOD = 5 
+
+# Regular mode
+FAST_SAMPLE_PERIOD = 10
+SLOW_SAMPLE_PERIOD = 180
+
+UPDATES_PER_READ = 5 # scale readings per mqtt result
+
+# Scale configurations
+scaleConfigs = [
+  # Scale 0 configuration
+  {'name': 'LeftFridgeLeftTap',
+   'data_gpio': 24,
+   'clk_gpio': 23,
+   'ref_unit': 24.675,
+   'tare_offset' : 8671108 },
+
+#  # Scale 1 configuration
+#  {'name': 'LeftFridgeRightTap',
+#   'data_gpio': 5,
+#   'clk_gpio': 6,
+#   'ref_unit': 24.675,
+#   'tare_offset' : 8671108 }
+  ]
+
+def cleanAndExit():
+    GPIO.cleanup()
+    myMQTTClient.unsubscribe("device/+/tare")
+    myMQTTClient.disconnect()
+    print ("%s: Bye!" % datetime.datetime.now())
+    sys.exit()
+
+def tareScale(scaleConfig):
+    scaleConfig['tare_offset'] = hx.tare()
+    print ("%s: Tare Value: %.2f" % (datetime.datetime.now(), scaleConfig['tare_offset']))
+
+def getScaleConfig(name):
+    global scaleConfigs
+    for config in scaleConfigs:
+        if (config['name'] == name):
+            return config
+
+def handleIncoming(client, userdata, message):
+    path = message.topic.split("/")
+    if (len(path) == 3):
+        if (path[2] == "tare"):
+            config = getScaleConfig(path[2])
+            if config is not None:
+                tareScale(config)
+            else:
+                print("Invalid Scale: %s" % path[2])
+        else:
+            print("Received unkown message topic: ")
+            print(message.topic)
+    else:
+        print("Received invalid topic name: ")
+        print(message.topic)
+    
+
+data = [[], []]
+mqtt = [[], []]
+updates = 0
+scaleDevices = []
+
+certdir = "aws_certs"
+if (len(sys.argv) > 1):
+    certdir = sys.argv[1];
+
+if ((not os.path.isfile(certdir + "/VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem")) or
+    (not os.path.isfile(certdir + "/affd2b97c0-private.pem.key")) or
+    (not os.path.isfile(certdir + "/affd2b97c0-certificate.pem.crt")) ):
+    print("Error: Could not find aws certificates in: " + certdir);
+    print("Usage: " + sys.argv[0] + " [aws certs dir]");
+    sys.exit(-1)
+
+sys.stdout = sys.stderr = open(LOGFILE, 'a')
+
+# Initialize Scales
+for config in scaleConfigs:
+    print("%s: Initializing %s (%d,%d) [%d/%d]" %
+          (datetime.datetime.now(), config['name'],
+           config['clk_gpio'], config['data_gpio'],
+           config['ref_unit'], config['tare_offset']))
+    hx = HX711(config['data_gpio'], config['clk_gpio'])
+    hx.set_reading_format("LSB", "MSB")
+    hx.set_reference_unit(config['ref_unit'])
+    hx.reset()
+    hx.set_tare(config['tare_offset'])
+    scaleDevices.append(hx)
+
+# Initialize MQTT session
+myMQTTClient = AWSIoTMQTTClient(THING)
+myMQTTClient.configureEndpoint(ENDPOINT, 8883)
+myMQTTClient.configureCredentials(certdir + "/VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem",
+                                  certdir + "/affd2b97c0-private.pem.key",
+                                  certdir + "/affd2b97c0-certificate.pem.crt")
+myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+myMQTTClient.connect()
+myMQTTClient.subscribe("device/+/tare", 1, handleIncoming)
+
+sleep_time = FAST_SAMPLE_PERIOD
+while True:
+    try:
+        updates += 1
+        # Read each scale
+        i = 0
+        while i < len(scaleDevices):
+            val = scaleDevices[i].get_weight(5)
+            data[i].append(val)
+
+            while len(data[i]) > UPDATES_PER_READ:
+                data[i].pop(0)
+
+            print ("%s: %s:  %.1f kg    [Val: %.2f  Size: %d/%d  period: %d]" %
+                    (datetime.datetime.now(), scaleConfigs[i]['name'], statistics.median(data[i]) / 1000.0,
+                     val, len(data[i]), UPDATES_PER_READ, sleep_time))
+            scaleDevices[i].power_down()
+            scaleDevices[i].power_up()
+            i += 1
+
+        if (updates % UPDATES_PER_READ) == 0:
+            # Send a single MQTT message with all of the scale readings
+            message = {}
+            j = 0
+            while j < len(scaleDevices):
+                scalename = scaleConfigs[j]['name']
+                timestamp = int(time.time())
+                weight = round(statistics.median(data[j])[0], 1)
+                message[scalename] = dict()
+                message[scalename]['group'] = GROUP
+                message[scalename]['thing'] = THING
+                message[scalename]['timestamp'] = timestamp
+                message[scalename]['weight'] = weight
+                messageJson = json.dumps(message)
+                j += 1
+            myMQTTClient.publish("device/" + THING + "/data", messageJson, 1)
+            print ("%s: MQTT send to deivce/%s/data: %s" % (datetime.datetime.now(), THING, messageJson))
+
+            mqtt[i].append(weight)
+            while len(mqtt[i]) > 5:
+                mqtt[i].pop(0)
+            if (len(mqtt[i]) > 1):
+                stdev = statistics.stdev(mqtt[i])
+                if (stdev > 10):
+                    sleep_time = FAST_SAMPLE_PERIOD
+                else:
+                    sleep_time = SLOW_SAMPLE_PERIOD
+                print ("stdev: %.2f  sample rate: %d" % (stdev, sleep_time));
+        sys.stdout.flush()
+        sys.stderr.flush()
+        os.fsync(sys.stdout.fileno())
+        os.fsync(sys.stderr.fileno())
+        time.sleep(sleep_time)
+    except (KeyboardInterrupt, SystemExit):
+        cleanAndExit()
