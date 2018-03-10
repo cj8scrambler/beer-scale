fa3a34ce8e2d71f6886c07455782e0a42109907e
Doug Zobel <zobel_junk@hotmail.com>
2018-03-10 17:02:40 -0600

    Move python scripts to subdir

---
+++ python/scaleconfig.py
@@ -0,0 +1,24 @@
+# Configuration Data
+
+#AWS IOT Thing Endpoint (Under AWS console IOT Thing: Interact)
+ENDPOINT = "a3f9rjliro4kji.iot.us-west-2.amazonaws.com"
+
+#AWS IOT thing name
+THING = "weight_pi"
+
+# Scale configurations
+scaleConfigs = [
+  # Scale 0 configuration
+  {'name': 'LeftFridgeLeftTap',
+   'data_gpio': 24,
+   'clk_gpio': 23,
+   'ref_unit': 24.675,
+   'tare_offset' : 8671108 },
+
+#  # Scale 1 configuration
+#  {'name': 'LeftFridgeRightTap',
+#   'data_gpio': 5,
+#   'clk_gpio': 6,
+#   'ref_unit': 24.675,
+#   'tare_offset' : 8671108 }
+  ]
