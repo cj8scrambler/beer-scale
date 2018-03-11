4a5683531b5d74d4fbc5a2277b4366b0f61b66bf
Doug Zobel <zobel_junk@hotmail.com>
2018-03-11 13:36:48 -0500

    lambda: Use promise to sync up async calls
    get_devices needs to wait for all of the thing data to come back before
    returning its result.
    
    iot2simpledb needs to wait for 2 paralell updates (simpledb & iot shadow)
    to finish before declaring success.  Ths works, but for some reason simpledb
    sometimes gets a double callback.

--- lambda/get_devices/index.js
+++ lambda/get_devices/index.js
@@ -1,5 +1,10 @@
 var AWS = require('aws-sdk');
 var iot = new AWS.Iot();
+var results = { 'results': [] };;
+
+// Could figure this out programaticly, but that takes time.
+// Comes from 'aws iot describe-endpoint'
+var ENDPOINT = "a3f9rjliro4kji.iot.us-west-2.amazonaws.com"
 
 exports.handler = (event, context, callback) => {
   //console.log('DZ: Received event:', event);
@@ -19,45 +24,45 @@ exports.handler = (event, context, callback) => {
     if (err) context.fail(err);
     else {
       //console.log('DZ: Received group list::', groupData);
-      iot.describeEndpoint({}, function(err, endpointData) {
-        if (err) context.fail(err);
-        else {
-          console.log('DZ: endpoint data:', endpointData);
-          groupData.things.forEach(function(item, index, array) {
-            console.log('DZ: on thing:', item)
-            var iotdata = new AWS.IotData({endpoint: endpointData.endpointAddress});
-            iotdata.getThingShadow({thingName: item}, function(err, shadowData) {
-              if (err) {
-                context.fail(err);
-              } else {
-                console.log('DZ:', item, "has:", shadowData)
-                var results = {};
-
-                results['results'] = [];
-                var jsonPayload = JSON.parse(shadowData.payload);
-                if (jsonPayload.state.reported != undefined) {
-                  for (var i in jsonPayload.state.reported.taps) {
-                    console.log('DZ: thing:', item, '  tap:', i, ' weight:', jsonPayload.state.reported.taps[i]);
-                    var entry = {};
-                    entry['tap'] = jsonPayload.state.reported.taps[i].name;
-                    entry['weight'] = jsonPayload.state.reported.taps[i].weight;
-                    console.log("DZ: entry is:", entry)
-                    results['results'].push(entry);
-                  }
+      groupData.things.forEach(function(item, index, array) {
+        console.log('DZ: on thing:', item)
+        var iotdata = new AWS.IotData({endpoint: ENDPOINT})
+        iotdata.getThingShadow({thingName: item}, function(err, shadowData) {
+          var shadowPromise = new Promise(function(resolve, reject) {
+            if (err) {
+              reject(err);
+            } else {
+              console.log('DZ:', item, "has:", shadowData)
+              var jsonPayload = JSON.parse(shadowData.payload);
+              if (jsonPayload.state.reported != undefined) {
+                for (var i in jsonPayload.state.reported.taps) {
+                  console.log('DZ: thing:', item, '  tap:', i, ' weight:', jsonPayload.state.reported.taps[i]);
+                  var entry = {};
+                  entry['tap'] = jsonPayload.state.reported.taps[i].name;
+                  entry['timestamp'] = jsonPayload.state.reported.taps[i].timestamp;
+                  entry['weight'] = jsonPayload.state.reported.taps[i].weight;
+                  console.log("DZ: entry is:", entry)
+                  results['results'].push(entry);
                 }
-
-                var response = {
-                  "statusCode": 200,
-                  "headers": {
-                  },
-                  "body": JSON.stringify(results),
-                  "isBase64Encoded": false
-                };
-                context.succeed(response);
+                resolve("Stuff worked!");
               }
-            });
+              reject(Error("No data found"));
+            }
           });
-        }
+          shadowPromise.then(function(result) {
+            console.log("DZ; promise result handler");
+            var response = {
+              "statusCode": 200,
+              "headers": {
+              },
+              "body": JSON.stringify(results),
+              "isBase64Encoded": false
+            };
+            context.succeed(response);
+          }, function(err) {
+            console.log("DZ; promise err handler");
+          });
+        });
       });
     }
   });
--- lambda/iot2simpledb/index.js
+++ lambda/iot2simpledb/index.js
@@ -1,8 +1,12 @@
 var AWS = require('aws-sdk');
 var iot = new AWS.Iot();
 
+// Could figure this out programaticly, but that takes time.
+// Comes from 'aws iot describe-endpoint'
+var ENDPOINT = "a3f9rjliro4kji.iot.us-west-2.amazonaws.com"
+
 exports.handler = (event, context, callback) => {
-//  console.log('DZ: Received event:', event);
+  console.log('DZ: Received event:', event);
   var simpledb = new AWS.SimpleDB();
   var params = {
     Items: []
@@ -11,11 +15,12 @@ exports.handler = (event, context, callback) => {
   var group;
   var thing;
   var taps = [];
+  var jobs = [];
   for (var scale in event){
     group = event[scale].group;
     thing = event[scale].thing;
-//    console.log('DZ: updated entry:', event[scale]);
-    taps.push({"name":  scale, "weight": event[scale].weight});
+    console.log('DZ: working on  entry:', event[scale]);
+    taps.push({"name":  scale, "weight": event[scale].weight, "timestamp": event[scale].timestamp});
     params.Items.push(
      { Attributes: [
         {
@@ -36,44 +41,45 @@ exports.handler = (event, context, callback) => {
   }
   params.DomainName = group;
   
-  //console.log('DZ: Writing record', JSON.stringify(params));
-  
-  simpledb.batchPutAttributes(params, function(err, data) {
+  console.log('DZ: pushing simpledb update job');
+  jobs.push(simpledb.batchPutAttributes(params, function(err, data) {
+    // For some reason, we hit this twice sometimes
+    console.log('DZ:   simbledb callback');
     if (err) {
-      console.log(err);
-      context.fail("Internal Error: " + JSON.stringify(err)); // an error occurred
+      console.log("DB Update Error: " + JSON.stringify(err));
     }
     else {
-//        console.log('DZ: Sent DB update', params);
-      context.succeed('DB update success: ' + params);
+      console.log(thing.toString() + " DB update success: " + JSON.stringify(params));
     }
-  });
+  }).promise());
   
   var update = {
     state: {
       reported: {
         taps
-        }
+      }
     }
   };
+  console.log('DZ; pushing shadow update job');
+  var iotdata = new AWS.IotData({endpoint: ENDPOINT});
+  jobs.push(iotdata.updateThingShadow(
+    {
+      payload: JSON.stringify(update),
+      thingName: thing.toString()
+    }, function(err, data) {
+      console.log('DZ;   shadow update callback');
+      if (err) {
+        console.log("Shadow Update failure:", err);
+      } else {
+        console.log(thing.toString() + " shadow update success:" + JSON.stringify(data));
+      }
+    }).promise());
 
-  iot.describeEndpoint({}, function(err, endpointData) {
-    if (err) console.log(err, err.stack);
-    else {
-      var iotdata = new AWS.IotData({endpoint: endpointData.endpointAddress});
-
-      iotdata.updateThingShadow(
-        {
-          payload: JSON.stringify(update),
-          thingName: thing.toString()
-        }, function(err, data) {
-          if (err) {
-            context.fail(err);
-          } else {
-//            console.log(data);
-            context.succeed('Shadow update success');
-          }
-      });
-    }
-  });
+  Promise.all(jobs).then(function() {
+      console.log("All jobs done");
+      context.succeed('data update success');
+    }, function(err) {
+      console.log("Problem somewhere: ", arguments);
+      context.fail(err);
+    });
 };
