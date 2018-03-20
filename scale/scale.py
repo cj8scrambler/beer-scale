1e06ddccfc8d8636f95ca2645c4eeade452ecda6
Doug Zobel <zobel_junk@hotmail.com>
2018-03-19 19:36:45 -0500

    scale: rename scale code directory and add command line configs
    Rename directory with scale code from 'python' to 'scale'.
    
    Add command line options to specify AWS certificates location
    and configuration file.

--- scale/scale.py
+++ scale/scale.py
@@ -2,17 +2,14 @@
 import sys
 import time
 import json
+import glob
 import statistics
 import datetime
+import argparse
 import os.path
 import RPi.GPIO as GPIO
 from hx711 import HX711  # https://github.com/tatobari/hx711py
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-
-from scaleconfig import scaleConfigs
-from scaleconfig import THING
-from scaleconfig import GROUP
-from scaleconfig import ENDPOINT
 
 LOGFILE = "/var/log/scale.log"
 
@@ -40,8 +37,8 @@ def tareScale(scaleConfig):
     print ("%s: Tare Value: %.2f" % (datetime.datetime.now(), scaleConfig['tare_offset']))
 
 def getScaleConfig(name):
-    global scaleConfigs
-    for config in scaleConfigs:
+    global settings
+    for config in settings['hx711']:
         if (config['name'] == name):
             return config
 
@@ -64,25 +61,54 @@ def handleIncoming(client, userdata, message):
 
 # Neet to allocate these dynamicaly based on number of scales
 data = [[], []]
-last_mqtt_report = [0,0];
+last_mqtt_report = [0,0]
 updates = 0
 scaleDevices = []
 
-certdir = "aws_certs"
-if (len(sys.argv) > 1):
-    certdir = sys.argv[1];
+parser = argparse.ArgumentParser(description='Read HX711 scale data and report to AWS IOT')
+parser.add_argument('-a', '--awscerts', default='aws_certs', help='directory containing AWS certificates')
+parser.add_argument('-c', '--config', default='scaleconfig.json', help='configuration file')
+args = parser.parse_args()
 
-if ((not os.path.isfile(certdir + "/VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem")) or
-    (not os.path.isfile(certdir + "/affd2b97c0-private.pem.key")) or
-    (not os.path.isfile(certdir + "/affd2b97c0-certificate.pem.crt")) ):
-    print("Error: Could not find aws certificates in: " + certdir);
-    print("Usage: " + sys.argv[0] + " [aws certs dir]");
+#Check AWS certificate files
+matches = glob.glob(args.awscerts + "/*.pem")
+if matches:
+    ca_cert = matches[0]
+else:
+    print("Error: Could not find certificate authority file in " + args.awscerts + "/")
+    sys.exit(-1)
+matches = glob.glob(args.awscerts + "/*private.pem.key")
+if matches:
+    priv_key = matches[0]
+else:
+    print("Error: Could not find private key file in " + args.awscerts + "/")
+    sys.exit(-1)
+matches = glob.glob(args.awscerts + "/*certificate.pem.crt")
+if matches:
+    cert = matches[0]
+else:
+    print("Error: Could not find certificate file in " + args.awscerts + "/")
     sys.exit(-1)
 
+#Load config file for AWS and HX711 interface settings
+try:
+    print("Loading settings from: " + args.config)
+    settings = json.load(open(args.config))
+    if (('hx711' not in settings) or
+        ('endpoint' not in settings) or
+        ('group' not in settings) or
+        ('thing' not in settings)):
+        print("Error: Invalid config file")
+        raise ImportError
+except:
+    print("Error: Could not load JSON configuratin file: " + args.config)
+    sys.exit(-1)
+
+#Redirect the rest to logfile
 sys.stdout = sys.stderr = open(LOGFILE, 'a')
 
 # Initialize Scales
-for config in scaleConfigs:
+for config in settings['hx711']:
     print("%s: Initializing %s (%d,%d) [%d/%d]" %
           (datetime.datetime.now(), config['name'],
            config['clk_gpio'], config['data_gpio'],
@@ -95,23 +121,21 @@ for config in scaleConfigs:
     scaleDevices.append(hx)
 
 # Initialize MQTT session
-myMQTTClient = AWSIoTMQTTClient(THING)
-myMQTTClient.configureEndpoint(ENDPOINT, 8883)
-myMQTTClient.configureCredentials(certdir + "/VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem",
-                                  certdir + "/affd2b97c0-private.pem.key",
-                                  certdir + "/affd2b97c0-certificate.pem.crt")
+myMQTTClient = AWSIoTMQTTClient(settings['thing'])
+myMQTTClient.configureEndpoint(settings['endpoint'], 8883)
+myMQTTClient.configureCredentials(ca_cert, priv_key, cert)
 myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 print ("%s: Connect to MQTT endpiont: %s  thing: %s" %
-       (datetime.datetime.now(), ENDPOINT, THING))
+       (datetime.datetime.now(), settings['endpoint'], settings['thing']))
 myMQTTClient.connect()
 myMQTTClient.subscribe("device/+/tare", 1, handleIncoming)
 
 while True:
     try:
-        weight_change = False;
+        weight_change = False
         updates += 1
         # Read each scale
         i = 0
@@ -123,14 +147,14 @@ while True:
                 data[i].pop(0)
 
             print ("%s: %s:  %.1f kg    [Val: %.2f  Size: %d/%d   update: %d/%.1f]" %
-                    (datetime.datetime.now(), scaleConfigs[i]['name'], statistics.median(data[i]) / 1000.0,
+                    (datetime.datetime.now(), settings['hx711'][i]['name'], statistics.median(data[i]) / 1000.0,
                      val, len(data[i]), MEDIAN_FILTER_SIZE, updates % MAX_SAMPLES_PER_MQTT, MAX_SAMPLES_PER_MQTT))
             scaleDevices[i].power_down()
             scaleDevices[i].power_up()
 
             if (abs(statistics.median(data[i]) - last_mqtt_report[i]) > MQTT_ALLOWED_VARIANCE):
-                print ("  Detected weight change on %s: delta: %.1f" % (scaleConfigs[i]['name'], statistics.median(data[i]) - last_mqtt_report[i]));
-                weight_change = True;
+                print ("  Detected weight change on %s: delta: %.1f" % (settings['hx711'][i]['name'], statistics.median(data[i]) - last_mqtt_report[i]))
+                weight_change = True
             i += 1
 
         if ((weight_change == True) or ((updates % MAX_SAMPLES_PER_MQTT) == 0)):
@@ -143,22 +167,22 @@ while True:
             while j < len(scaleDevices):
                 weight = round(statistics.median(data[j]), 1)
                 datapoint = dict()
-                datapoint['tap'] = scaleConfigs[j]['name']
-                datapoint['thing'] = THING
-                datapoint['group'] = GROUP
+                datapoint['tap'] = settings['hx711'][j]['name']
+                datapoint['thing'] = settings['thing']
+                datapoint['group'] = settings['group']
                 datapoint['timestamp'] = int(time.time())
                 datapoint['weight'] = weight
 
                 update['state']['reported']['taps'].append(datapoint)
 
-                last_mqtt_report[j] = weight;
+                last_mqtt_report[j] = weight
 
                 j += 1
             messageJson = json.dumps(update)
-            myMQTTClient.publish("$aws/things/" + THING + "/shadow/update", messageJson, 1)
+            myMQTTClient.publish("$aws/things/" + settings['thing'] + "/shadow/update", messageJson, 1)
             print ("%s: MQTT send to %s: %s" % (datetime.datetime.now(),
-                   "$aws/things/" + THING + "/shadow/update", messageJson))
-            updates = 0;
+                   "$aws/things/" + settings['thing'] + "/shadow/update", messageJson))
+            updates = 0
 
 
         sys.stdout.flush()
